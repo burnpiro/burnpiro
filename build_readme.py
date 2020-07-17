c4038e28abb0340d4afff8cb6beadbd22550b51d
burnpiro <kemalpiro@gmail.com>
2020-07-18 00:53:41 +0200

    create build script

---
+++ build_readme.py
@@ -0,0 +1,39 @@
+import feedparser
+import pathlib
+import re
+import os
+
+root = pathlib.Path(__file__).parent.resolve()
+
+
+def replace_chunk(content, marker, chunk):
+    r = re.compile(
+        r"<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->".format(marker, marker),
+        re.DOTALL,
+    )
+    chunk = "<!-- {} starts -->\n{}\n<!-- {} ends -->".format(marker, chunk, marker)
+    return r.sub(chunk, content)
+
+
+def fetch_blog_entries():
+    return [
+        {
+            "title": entry["title"],
+            "url": entry["id"],
+            "published": entry["published"],
+        }
+        for entry in feedparser.parse("https://erdem.pl/rss.xml")["entries"]
+    ]
+
+
+if __name__ == "__main__":
+    readme = root / "README.md"
+    readme_contents = readme.open().read()
+
+    entries = fetch_blog_entries()[:5]
+    entries_md = "\n".join(
+        ["* [{title}]({url}) - {published}".format(**entry) for entry in entries]
+    )
+    rewritten = replace_chunk(readme_contents, "blog", entries_md)
+
+    readme.open("w").write(rewritten)
